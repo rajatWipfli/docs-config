d25982dbd66a68630bded9d475cb8bab70503733
rajatwipfli <rajat.parashar@wipfli.com>
2022-07-19 20:14:04 +0530

    update checking samplefiles

--- runner.js
+++ runner.js
@@ -13,7 +13,7 @@ function createNecessaryFolders(root, config) {
   }
   const tutorialPath = !path.isAbsolute(config.opts.tutorials) ? path.join(root, config.opts.tutorials) : config.opts.tutorials;
   // Skip creation of files when folder already exists
-  if (fs.existsSync(tutorialPath)) {
+  if (checkIfSetupExists(root, config)) {
     return;
   }
   necessaryFolders.forEach(folderName => {
@@ -27,10 +27,24 @@ function createNecessaryFolders(root, config) {
 function createSampleFiles(root, config) {
   const tutorialPath = !path.isAbsolute(config.opts.tutorials) ? path.join(root, config.opts.tutorials) : config.opts.tutorials;
   // Skip creation of files when folder already exists
-  if (fs.existsSync(tutorialPath)) {
+  if (checkIfSetupExists(root, config)) {
+    return;
+  }
+  if(fs.existsSync(path.join(tutorialPath, 'sample.md'))){
     return;
   }
   fs.copyFileSync(path.resolve(__dirname, './sample.md'), path.join(tutorialPath, 'sample.md'));
+}
+
+function checkIfSetupExists(root, config) {
+  const tutorialPath = !path.isAbsolute(config.opts.tutorials) ? path.join(root, config.opts.tutorials) : config.opts.tutorials;
+  if (!fs.existsSync(tutorialPath)) {
+    return false;
+  }
+  const listFiles = fs.readdirSync(tutorialPath, {withFileTypes: true});
+  const sampleFiles = necessaryFolders.map(folderFileName => folderFileName.replace('--documentation--', '')).concat(['sample.md'])
+  // Check is the tutorial folder has any file other that sampleFiles, then setup file exists
+  return listFiles.some(file => sampleFiles.some(folderFileName => !file.name.endsWith(folderFileName)));
 }
 
 function run() {
